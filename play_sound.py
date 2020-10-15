1f329711680ed633e9a75dd4264e17de0eaecd85
ioEl0xFF <syeiku13@yahoo.co.jp>
2020-10-15 13:48:18 +0900

    second commit

--- play_sound.py
+++ play_sound.py
@@ -5,17 +5,22 @@ import time
 import os
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__)) + '/music_data/'
-
+ms = 1000
 def play_sound(queue):
   old_filename = ''
   while True:
     id = queue.get()
     if id == 'quit':
+      pygame.mixer.music.fadeout(ms)
+      time.sleep(ms / 1000)
       pygame.mixer.music.stop()
       return
     filename = FILE_DIR + id + '.mp3' #再生したいmp3ファイル
     if filename != old_filename:
       #pygame.mixer.music.stop()
+      if old_filename != '':
+        pygame.mixer.music.fadeout(ms)
+        time.sleep(ms / 1000)
       old_filename = filename
 
       pygame.mixer.init()
@@ -23,4 +28,3 @@ def play_sound(queue):
       mp3_length = mp3(filename).info.length #音源の長さ取得
       pygame.mixer.music.play(-1) #再生開始。1の部分を変えるとn回再生(-1で無限ループ)
       #time.sleep(mp3_length + 0.25) #再生開始後、音源の長さだけ待つ(0.25待つのは誤差解消)
-
